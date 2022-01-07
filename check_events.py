dc1f197e99e8b81990a7411352fab34606ae072a
Malcolm van Raalte <malcolm@van.raalte.ca>
2022-01-07 14:46:21 -0500

    IMMINENT_MINUTES

--- check_events.py
+++ check_events.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from os import getenv
 from update_ui import show_imminent_event
 
 with open("events.txt", "r") as f:
@@ -7,7 +8,7 @@ with open("events.txt", "r") as f:
 now = datetime.now(timezone.utc)
 imminent_events = [
     ev for ev in event_times
-    if ev > now and ev < (now + timedelta(minutes=5))
+    if ev > now and ev < (now + timedelta(minutes=int(getenv("IMMINENT_MINUTES"))))
 ]
 
 show_imminent_event(bool(imminent_events))
